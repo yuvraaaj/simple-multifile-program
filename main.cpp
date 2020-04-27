477b987248ed077ac57ea7aed8740cef1b21ea37
Yuvraj Manral <42829486+yuvrajmanral@users.noreply.github.com>
2020-04-27 22:43:42 +0530

    updated-main.cpp
    Shifting the user-defined function declaration in another file.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-
-// Add declarations here:
-double average(double num1, double num2);
-int tenth_power(int num);
-bool is_palindrome(std::string text);
+#include "fns.hpp"
 
 int main() {
   
-  std::cout << is_palindrome("racecar") << "\n";
-  std::cout << tenth_power(3) << "\n";
-  std::cout << average(8.0, 19.0) << "\n";
+  std::cout << is_palindrome("noon") << "\n";
+  std::cout << tenth_power(4) << "\n";
+  std::cout << average(4.0, 7.0) << "\n";
   
 }
